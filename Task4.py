c8b99e19a3c701926f3877db722db4791d362915
SSasha88 <d1rus1853@gmail.com>
2024-11-02 09:51:54 +0300

    Add files via upload

---
+++ Task4.py
@@ -0,0 +1,8 @@
+print("4th program")  #"Первый после точки"
+print("123.456")
+print(type("123.456"))
+print(float("123.456")*10)
+print(type(float("123.456")*10))
+print(int(float("123.456")*10))
+print(type(int(float("123.456")*10)))
+print(int(float("123.456")*10)%5)    # решение
---
+++ task3.py
@@ -0,0 +1,7 @@
+print("3rd program")
+print("2*2+2")
+print(2*2+2)
+print("2*(2+2)")
+print(2*(2+2))
+print("(2*2+2)==(2*(2+2)")
+print((2*2+2)==(2*(2+2)))
